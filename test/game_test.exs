629ea1be6e1e4b0e346eb6c5ee4fd1aad6bb9919
Mark <markchav3z@gmail.com>
2017-08-04 20:16:10 +0800

    update etst

--- test/game_test.exs
+++ test/game_test.exs
@@ -9,4 +9,14 @@ defmodule GameTest do
     assert game.game_state == :initializing
     assert length(game.letters) > 0
   end
+
+  test "all letters should be in lowercase" do
+    game = Hangman.Game.init()
+
+    result = game.letters
+    |> Enum.map(&(String.downcase(&1) == &1))
+    |> Enum.all?(fn(_x) -> true end)
+
+    assert result == true
+  end
 end
